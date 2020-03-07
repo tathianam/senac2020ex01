96b56b4e1fd7e570084d9268356ae82afe9b2c8d
TATHIANA MATOS MACHADO <tathiana.mmachado@senacsp.edu.br>
2020-03-06 22:10:16 -0300

    pagina detalhe

---
+++ src/app/artigo/artigo-detalhe/artigo-detalhe.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artigo-detalhe',
+  templateUrl: './artigo-detalhe.page.html',
+  styleUrls: ['./artigo-detalhe.page.scss'],
+})
+export class ArtigoDetalhePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
